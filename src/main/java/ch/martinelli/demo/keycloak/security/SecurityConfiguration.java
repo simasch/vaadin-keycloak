3696b34fe277ea914df68e721b84a9d76fbe47d6
Simon Martinelli <simon@72.services>
2022-11-20 16:22:32 +0100

    Added role mapping

--- src/main/java/ch/martinelli/demo/keycloak/security/SecurityConfiguration.java
+++ src/main/java/ch/martinelli/demo/keycloak/security/SecurityConfiguration.java
@@ -5,10 +5,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @EnableWebSecurity
 @Configuration
@@ -25,6 +34,31 @@ public class SecurityConfiguration extends VaadinWebSecurity {
     @Bean
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    public GrantedAuthoritiesMapper userAuthoritiesMapperForKeycloak() {
+        return authorities -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+            var authority = authorities.iterator().next();
+
+            if (authority instanceof OidcUserAuthority oidcUserAuthority) {
+                var userInfo = oidcUserAuthority.getUserInfo();
+
+                if (userInfo.hasClaim("realm_access")) {
+                    var realmAccess = userInfo.getClaimAsMap("realm_access");
+                    var roles = (Collection<String>) realmAccess.get("roles");
+                    mappedAuthorities.addAll(generateAuthoritiesFromClaim(roles));
+                }
+            }
+            return mappedAuthorities;
+        };
+    }
+
+    Collection<GrantedAuthority> generateAuthoritiesFromClaim(Collection<String> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toUpperCase()))
+                .collect(Collectors.toList());
     }
 
     @Override
